e9be2ec4cd7b778f7d7ca2d2267eb9d2c79a8d59
Hugo de Lelis <hugodelelis00@gmail.com>
2024-06-08 20:55:30 -0300

    add script

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const data = new Date()
+const currentYear = data.getFullYear()
+const currentMonth = data.getMonth() + 1
+const currentDay = data.getDate()
+const yesterday = data.getFullYear()
+console.log(yesterday)
+
+const addZero = (num) => {
+    return num < 10 ? `0${num}` : `${num}`
+}
+
+const results = () => {
+    const day = Number(document.querySelector('#day').value);
+    const month = Number(document.querySelector('#month').value);
+    const year = Number(document.querySelector('#year').value);
+
+    let yearResult 
+    let monthResult 
+    let dayResult 
+    
+    currentYear > year ? yearResult = (year - currentYear) * (-1) : yearResult = (year - currentYear)
+    document.querySelector('#result-years').innerHTML = addZero(yearResult-1)
+
+    currentMonth > month ? monthResult = (month - currentMonth) * (-1) : monthResult = (month - currentMonth)
+    document.querySelector('#result-months').innerHTML = monthResult  
+
+    currentDay > day ? dayResult = (day - currentDay) * (-1) : dayResult = (day - currentDay)
+    document.querySelector('#result-days').innerHTML = addZero(dayResult) 
+    
+    if (month <= currentMonth && day <= currentDay && year <= currentYear) {
+        document.querySelector('#result-years').innerHTML = addZero(yearResult)
+    }
+}
+
